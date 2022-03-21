f726a16770d8ca42af5871ee7f998a50b812f9c7
Micko Widi <widimicko85@gmail.com>
2022-03-21 15:08:37 +0700

    add validation message to login/register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,61 +5,93 @@
 @endpush
 
 <main>
-    <div class="container">
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+  <div class="container">
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-              <div class="d-flex justify-content-center py-4">
-                <a href="#" class="logo d-flex align-items-center w-auto">
-                  <span class="d-none d-lg-block">Maesa Group</span>
-                </a>
+            <div class="d-flex justify-content-center py-4">
+              <a href="#" class="logo d-flex align-items-center w-auto">
+                <span class="d-none d-lg-block">Maesa Group</span>
+              </a>
+            </div>
+
+            @if ($notification = Session::get('success'))
+              <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ $notification }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>
+            @endif
 
-              <div class="card mb-3">
-                <div class="card-body">
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
-                  </div>
-                  <form class="row g-3" action="{{ route('login') }}" method="POST">
-                      @csrf
-                    <div class="col-12">
-                      <label for="yourUsername" class="form-label">Email</label>
-                      <div class="input-group has-validation">
-                        <input type="email" name="email" class="form-control" required>
-                        <div class="invalid-feedback">Please enter your email.</div>
-                      </div>
-                    </div>
-                    <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" required>
-                      <div class="invalid-feedback">Please enter your password!</div>
-                    </div>
-                    <div class="col-12">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" value="true">
-                        <label class="form-check-label" for="rememberMe">Remember me</label>
-                      </div>
-                    </div>
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Login</button>
-                    </div>
-                    <div class="col-12">
-                      <p class="small mb-0">Don't have account? <a href="{{ route('register') }}">Create an account</a></p>
-                    </div>
-                  </form>
+            @if ($notification = Session::get('failed'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ $notification }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+            @endif
+
+            @if ($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              </div>
+            @endif
+
+            <div class="card mb-3">
+              <div class="card-body">
+                <div class="pt-4 pb-2">
+                  <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                  <p class="text-center small">Enter your username & password to login</p>
                 </div>
+                <form class="row g-3" action="{{ route('login') }}" method="POST">
+                    @csrf
+                  <div class="col-12">
+                    <label class="form-label">Email</label>
+                    <div class="input-group has-validation">
+                      <input type="email" name="email" class="form-control" required>
+                      @error('email')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <label class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" required>
+                    @error('password')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" name="remember" value="true">
+                      <label class="form-check-label" for="rememberMe">Remember me</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button class="btn btn-primary w-100" type="submit">Login</button>
+                  </div>
+                  <div class="col-12">
+                    <p class="small mb-0">Don't have account? <a href="{{ route('register') }}">Create an account</a></p>
+                  </div>
+                </form>
               </div>
-              <div class="credits">
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-              </div>
+            </div>
+            <div class="credits">
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+  </div>
 </main>
 
 @push('script')
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,74 +5,87 @@
 @endpush
 
 <main>
-    <div class="container">
+  <div class="container">
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+            <div class="d-flex justify-content-center py-4">
+              <a href="#" class="logo d-flex align-items-center w-auto">
+                <span class="d-none d-lg-block">Maesa Group</span>
+              </a>
+            </div>
 
-              <div class="d-flex justify-content-center py-4">
-                <a href="#" class="logo d-flex align-items-center w-auto">
-                  <span class="d-none d-lg-block">Maesa Group</span>
-                </a>
-              </div>
+            <div class="card mb-3">
+              <div class="card-body">
+                <div class="pt-4 pb-2">
+                  <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+                  <p class="text-center small">Enter your personal details to create account</p>
+                </div>
 
-              <div class="card mb-3">
-                <div class="card-body">
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
-                    <p class="text-center small">Enter your personal details to create account</p>
+                <form class="row g-3 needs-validation" action="{{ route('register') }}" method="POST">
+                  @csrf
+                  <div class="col-12">
+                    <label class="form-label">Your Name</label>
+                    <input type="text" name="name" class="form-control" required>
+                    @error('name')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
                   </div>
 
-                  <form class="row g-3 needs-validation" action="{{ route('register') }}" method="POST">
-                    @csrf
-                    <div class="col-12">
-                      <label class="form-label">Your Name</label>
-                      <input type="text" name="name" class="form-control" required>
-                      <div class="invalid-feedback">Please, enter your name!</div>
-                    </div>
-
-                    <div class="col-12">
-                      <label class="form-label">Your Email</label>
-                      <input type="email" name="email" class="form-control" required>
-                      <div class="invalid-feedback">Please enter a valid Email adddress!</div>
-                    </div>
-
-                    <div class="col-12">
-                      <label class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" required>
-                      <div class="invalid-feedback">Please enter your password!</div>
-                    </div>
-
-                    <div class="col-12">
-                        <label class="form-label">Password Confirmation</label>
-                        <input type="password" name="password_confirmation" class="form-control" required>
-                        <div class="invalid-feedback">Please re-enter your password!</div>
+                  <div class="col-12">
+                    <label class="form-label">Your Email</label>
+                    <input type="email" name="email" class="form-control" required>
+                    @error('email')
+                      <div class="invalid-feedback">
+                        {{ $message }}
                       </div>
+                    @enderror
+                  </div>
 
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Create Account</button>
-                    </div>
-                    <div class="col-12">
-                      <p class="small mb-0">Already have an account? <a href="{{ route('login') }}">Log in</a></p>
-                    </div>
-                  </form>
+                  <div class="col-12">
+                    <label class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" required>
+                    @error('password')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
 
-                </div>
+                  <div class="col-12">
+                    <label class="form-label">Password Confirmation</label>
+                    <input type="password" name="password_confirmation" class="form-control" required>
+                    @error('password_confirmation')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+
+                  <div class="col-12">
+                    <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                  </div>
+                  <div class="col-12">
+                    <p class="small mb-0">Already have an account? <a href="{{ route('login') }}">Log in</a></p>
+                  </div>
+                </form>
+
               </div>
-
-              <div class="credits">
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-              </div>
-
             </div>
+
+            <div class="credits">
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+
           </div>
         </div>
-
-      </section>
-
-    </div>
+      </div>
+    </section>
+  </div>
 </main>
 
 @push('script')
